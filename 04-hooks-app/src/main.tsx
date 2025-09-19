5fe8b91e42a5618392e12ab89c8ca027ff42570f
Willy Arrojas <willyarrojas@gmail.com>
2025-09-19 12:51:24 +0200

    uso de useTransition y useOptimistic

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MemoCounter } from "./06-memos/MemoCounter";
+import { Toaster } from "sonner";
+import { InstagromApp } from "./07-useOptimistic/InstagromApp";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Toaster></Toaster>
     {/* <HooksApp></HooksApp> */}
     {/* <TrafficLight></TrafficLight> */}
     {/* <TrafficLightWithEffect></TrafficLightWithEffect> */}
@@ -14,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TasksApp></TasksApp> */}
     {/* <ScrambleWords></ScrambleWords> */}
     {/* <MemoHook></MemoHook> */}
-    <MemoCounter></MemoCounter>
+    {/* <MemoCounter></MemoCounter> */}
+    <InstagromApp></InstagromApp>
   </StrictMode>
 );
